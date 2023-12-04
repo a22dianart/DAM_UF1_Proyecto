84c126fe77f7020899d53fc84696e49d8e924131
Diana <a22dianart@iessanclemente.net>
2023-12-04 01:32:26 +0100

    falta My Trips

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/where/countries/CountryViewHolder.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/where/countries/CountryViewHolder.kt
@@ -1,20 +1,34 @@
 package com.example.uf1_proyecto.where.countries
 
+import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.uf1_proyecto.R
+import com.example.uf1_proyecto.databinding.ItemCityBinding
+import com.example.uf1_proyecto.databinding.ItemCountryBinding
 import com.example.uf1_proyecto.where.countries.Country
 
 class CountryViewHolder(view: View): RecyclerView.ViewHolder(view){
 
-    val nameCountry = view.findViewById<TextView>(R.id.countryName)
-    val imageCountry = view.findViewById<ImageView>(R.id.countryImage)
-    fun render(countryModel: Country){
-        nameCountry.text=countryModel.name
-        Glide.with(imageCountry.context).load(countryModel.photo).into(imageCountry)
+    val binding = ItemCountryBinding.bind(view)
+    val view2 = view
 
+    fun render(countryModel: Country){
+        binding.countryName.text=countryModel.name
+        Glide.with(binding.countryImage.context).load(countryModel.photo).into(binding.countryImage)
+
+
+
+        binding.countryImage.setOnClickListener{
+
+            val bundle = Bundle()
+            bundle.putString("countryName", countryModel.name)
+
+            view2.findNavController().navigate(R.id.action_countriesFragment_to_selectedCountryFragment, bundle)
+        }
     }
 }
--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/where/countries/SelectedCountryFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/where/countries/SelectedCountryFragment.kt
@@ -1,27 +1,112 @@
 package com.example.uf1_proyecto.where.countries
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.example.uf1_proyecto.MainActivity
+import com.example.uf1_proyecto.Trip
+import com.example.uf1_proyecto.databinding.FragmentSelectedCityBinding
+import com.example.uf1_proyecto.databinding.FragmentSelectedCountryBinding
 
-import com.example.uf1_proyecto.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SelectedCountryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SelectedCountryFragment : Fragment() {
+    private var _binding: FragmentSelectedCountryBinding? = null
+    private val binding get() = _binding!!
 
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selected_country, container, false)
+        _binding = FragmentSelectedCountryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val mainactivity = requireActivity() as MainActivity
+
+        // Recupera el nombre de la ciudad del argumento
+        val paises: List<Country> = mainactivity.pasarPaises()
+        val countryName = arguments?.getString("countryName")!!
+        val pais = obtainCountry(paises, countryName)!! //vai estar si ou si xa que fixen click sobre ese pais
+
+        binding.collapsingToolbar.title=pais.name
+        Glide.with(binding.placeImage.context).load(pais.photo).into(binding.placeImage) //cargamos a imaxe de internet
+        binding.descriptionPlace.text=pais.description
+
+        //Poñemos as actividades
+        binding.activity1txt.text=pais.activities.get(0)
+        binding.activity2txt.text=pais.activities.get(1)
+        binding.activity3txt.text=pais.activities.get(2)
+        binding.activity4txt.text=pais.activities.get(3)
+        binding.activity5txt.text=pais.activities.get(4)
+        binding.activity6txt.text=pais.activities.get(5)
+
+        //Poñemos os sitios
+        binding.place1txt.text=pais.places.get(0)
+        binding.place2txt.text=pais.places.get(1)
+        binding.place3txt.text=pais.places.get(2)
+        binding.place4txt.text=pais.places.get(3)
+        binding.place5txt.text=pais.places.get(4)
+        binding.place6txt.text=pais.places.get(5)
+
+
+        //Poñemos a comida
+        binding.typical1txt.text=pais.typical.get(0)
+        binding.typical2txt.text=pais.typical.get(1)
+        binding.typical3txt.text=pais.typical.get(2)
+        binding.typical4txt.text=pais.typical.get(3)
+
+
+        //Para poñer o texto que di o páis no que está e as linguas que se falan
+        val configuration = resources.configuration
+        val lingua = configuration.locales[0].language
+
+        val linguas =pais.languages
+        if (lingua.equals("gl")){
+            binding.languages.text="Fálase $linguas"
+        }else if (lingua.equals("es")){
+            binding.languages.text="Se habla $linguas"
+        } else{
+            binding.languages.text="People speak $linguas"
+        }
+
+
+
+        binding.addButton.setOnClickListener{
+            var mensaxe : String
+            if (lingua.equals("gl")){
+                mensaxe="$countryName engadido a As miñas viaxes"
+            }else if (lingua.equals("es")){
+                mensaxe="$countryName añadido a Mis viajes"
+            } else{
+                mensaxe="$countryName added to My trips"
+            }
+
+
+            val newTrip = Trip(
+                name = countryName,
+                photo =pais.photo
+            )
+
+            // Insertar el nuevo Trip utilizando el ViewModel
+
+            Toast.makeText(binding.addButton.context, mensaxe, Toast.LENGTH_SHORT).show()
+
+        }
+
+        return view
     }
 
+    /**
+     * Función para obter a cidade na que fixemos click
+     */
+    fun obtainCountry(paises: List<Country>, nombre: String): Country? {
+        return paises.find { it.name == nombre }
+    }
 
 }
